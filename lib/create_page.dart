d1a5ec9c26ed99b5966075438501b14004b49554
supercoder <kazehazaya@gmail.com>
2020-12-14 18:45:02 +0900

    buildAdding

---
+++ lib/create_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CreatePage extends StatefulWidget {
+  @override
+  _CreatePageState createState() => _CreatePageState();
+}
+
+class _CreatePageState extends State<CreatePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppbar(),
+    );
+  }
+
+  _buildAppbar() {
+    
+  }
+}
