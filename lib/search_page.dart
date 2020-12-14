92d903389a16d6e90321ee122f33e3ef4e15e5fc
supercoder <kazehazaya@gmail.com>
2020-12-14 18:55:45 +0900

    create_page_button

--- lib/search_page.dart
+++ lib/search_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'create_page.dart';
+
 class SearchPage extends StatefulWidget {
   @override
   _SearchPageState createState() => _SearchPageState();
@@ -11,7 +13,11 @@ class _SearchPageState extends State<SearchPage> {
     return Scaffold(
       body: _buildBody(),
       floatingActionButton: FloatingActionButton(
-        onPressed: null,
+        onPressed: (){
+          Navigator.push(context, MaterialPageRoute(
+            builder: (context) => CreatePage()
+          ));
+        },
         child: Icon(Icons.create),
         backgroundColor: Colors.blue,
       ),
