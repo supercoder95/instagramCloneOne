311d6a78abba0d6217217ca218e6218e7bbcea9a
supercoder <kazehazaya@gmail.com>
2020-12-17 16:55:39 +0900

    a

--- lib/create_page.dart
+++ lib/create_page.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class CreatePage extends StatefulWidget {
   @override
@@ -8,20 +11,22 @@ class CreatePage extends StatefulWidget {
 class _CreatePageState extends State<CreatePage> {
   final textEditingController = TextEditingController();
 
+  File _image;
+
   @override
   void dispose() {
     textEditingController.dispose();
     super.dispose();
   }
-
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppbar(),
       body: _buildBody(),
       floatingActionButton: FloatingActionButton(
-        onPressed: null,
+        onPressed: _getImage,
+        child: Icon(Icons.add_a_photo),
       ),
     );
   }
@@ -42,12 +47,21 @@ class _CreatePageState extends State<CreatePage> {
   Widget _buildBody() {
     return Column(
       children: <Widget>[
-        Text('No Image'),
+        _image == null ? Text('No Image') : Image.file(_image),
         TextField(
           decoration: InputDecoration(hintText: '내용을 입력하세요'),
           controller: textEditingController,
         )
       ],
     );
+  }
+
+  Future<void> _getImage() async {
+    var image = await ImagePicker.pickImage(
+        source: ImageSource.gallery
+    );
+    setState(() {
+      _image = image;
+    });
   }
 }
