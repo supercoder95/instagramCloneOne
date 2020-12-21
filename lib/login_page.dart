9ad942008ffb1fdb7d1ec77a68c123c240973560
supercoder <kazehazaya@gmail.com>
2020-12-21 19:17:09 +0900

    a

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,9 +1,19 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
-class LoginPage extends StatelessWidget {
+import 'tab_page.dart';
+
+// 폰트: https://lingojam.com/FontsForInstagram
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   final GoogleSignIn _googleSignIn = GoogleSignIn();
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
@@ -18,32 +28,30 @@ class LoginPage extends StatelessWidget {
               'Instagram Clon',
               style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
             ),
-            Padding(
-              padding: EdgeInsets.all(50.0),
+            Container(
+              margin: EdgeInsets.all(50.0),
             ),
-            SignInButton(Buttons.Google,
-                onPressed: () {
-                  _handleSignIn()
-                      .then((FirebaseUser user) => print(user));
+            SignInButton(
+              Buttons.Google,
+              onPressed: () {
+                _handleSignIn().then((user) {
+                  print(user);
+                });
               },
-            )
+            ),
           ],
         ),
       ),
     );
   }
 
-  Future<FirebaseUser> _handleSignIn() async{
-    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
-    final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-
-    final AuthCredential credential = GoogleAuthProvider.getCredential(
-        idToken: googleAuth.idToken,
-        accessToken: googleAuth.accessToken,
-    );
-
-    final FirebaseUser user = (await _auth.signInWithCredential(credential)).user;
-    print("sign in " + user.displayName);
+  Future<FirebaseUser> _handleSignIn() async {
+    GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+    FirebaseUser user = (await _auth.signInWithCredential(
+        GoogleAuthProvider.getCredential(
+            idToken: googleAuth.idToken, accessToken: googleAuth.accessToken))).user;
+    print("signed in " + user.displayName);
     return user;
   }
 }
