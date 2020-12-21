d33ecb0e8d7cadd86b6242974f6c053714bc6e56
supercoder95 <supercoder@kakao.com>
2020-12-21 16:52:34 +0900

    changed firebase code

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_signin_button/button_list.dart';
-import 'package:flutter_signin_button/button_view.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:instagram_clone/tab_page.dart';
 
 class LoginPage extends StatelessWidget {
   final GoogleSignIn _googleSignIn = GoogleSignIn();
-  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
@@ -16,23 +14,18 @@ class LoginPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text('Instagram Clon',
-              style: TextStyle(
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.bold
-              ),
+            Text(
+              'Instagram Clon',
+              style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
             ),
             Padding(
               padding: EdgeInsets.all(50.0),
             ),
-            SignInButton(
-                Buttons.Google, onPressed: (){
-                  _handleSignIn().then(
-                          (user) => Navigator.pushReplacement(context, MaterialPageRoute(
-                            builder: (context) => TabPage(user)
-                          ))
-                  );
-              }
+            SignInButton(Buttons.Google,
+                onPressed: () {
+                  _handleSignIn()
+                      .then((FirebaseUser user) => print(user));
+              },
             )
           ],
         ),
@@ -40,14 +33,17 @@ class LoginPage extends StatelessWidget {
     );
   }
 
-  Future<FirebaseUser> _handleSignIn() async {
-    GoogleSignInAccount googleUser = await _googleSignIn.signIn();
-    GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-    FirebaseUser user = await _firebaseAuth.signInWithCredential(
-        GoogleAuthProvider.getCredential(
-            idToken: googleAuth.idToken, accessToken: googleAuth.accessToken
-      )
+  Future<FirebaseUser> _handleSignIn() async{
+    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+        idToken: googleAuth.idToken,
+        accessToken: googleAuth.accessToken,
     );
+
+    final FirebaseUser user = (await _auth.signInWithCredential(credential)).user;
+    print("sign in " + user.displayName);
     return user;
   }
 }
--- lib/root_page.dart
+++ lib/root_page.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:instagram_clone/login_page.dart';
-import 'package:instagram_clone/tab_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'loading_page.dart';
+
+import 'login_page.dart';
+import 'tab_page.dart';
 
 class RootPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    print('root_page created');
+    return _handleCurrentScreen();
+  }
+
+  Widget _handleCurrentScreen() {
     return StreamBuilder<FirebaseUser>(
+      stream: FirebaseAuth.instance.onAuthStateChanged,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return LoadingPage();
+        } else {
+          if (snapshot.hasData) {
+            return TabPage(snapshot.data);
+          }
+          return LoginPage();
+        }
+      },
     );
   }
 }
